7d3862c702f8aca435dc0a266a965ceeedd5496f
krokowski0x <krokowski.dx@gmail.com>
2018-06-07 10:58:04 +0200

    Added basic script for testing hardware

---
+++ raspi/index.js
@@ -0,0 +1,29 @@
+const five = require("johnny-five");
+const Raspi = require("raspi-io");
+const board = new five.Board({
+  io: new Raspi(),
+});
+
+const cryptos = ['BTC 1000.45', 'ETH 700,89', 'LTC 89.76'];
+let i = 0;
+
+board.on("ready", function() {
+  const motion = new five.Motion(8);
+  const lcd = new five.LCD({
+    pins: [2, 3, 4, 5, 6, 7],
+    backlight: 3,
+    rows: 2,
+    cols: 16,
+  });
+
+  lcd.clear().print('Hello!');
+  motion.on("calibrated", function() {
+    console.log("calibrated");
+  });
+
+  motion.on("change", function() {
+    lcd.clear().print(cryptos[i]);
+    i === 2 ? i = 0 : i++;
+  });
+
+});
