1bf3d4fbeef6d755450edf76ee29d53e39f0770f
krokowski0x <krokowski.dx@gmail.com>
2018-06-17 15:13:20 +0200

    Updated hardware setup after testing in real life

--- raspi/hardwareSetup.js
+++ raspi/hardwareSetup.js
@@ -4,28 +4,20 @@ const board = new five.Board({
   io: new Raspi(),
 });
 
-const cryptos = ['BTC 1000.45', 'ETH 700,89', 'LTC 89.76'];
-let i = 0;
-
 board.on("ready", function() {
-  const motion = new five.Motion('P1-8');
   const lcd = new five.LCD({
-    pins: ['P1-37', 'P1-35', 'P1-33', 'P1-31', 'P1-29', 'P1-23'],
-    backlight: 3,
+    pins: ['P1-37', 'P1-35', 'P1-33', 'P1-31', 'P1-29', 'P1-38'],
+    backlight: 6,
     rows: 2,
     cols: 16,
   });
-  const led = new five.Led('P1-7');
-
-  led.blink();
-  lcd.clear().print('Hello!');
-  motion.on("calibrated", function() {
-    console.log("calibrated");
-  });
-
-  motion.on("change", function() {
-    lcd.clear().print(cryptos[i]);
-    i === 2 ? i = 0 : i++;
-  });
-
+  lcd.clear();
 });
+// const motion = new five.Motion('P1-8');
+// motion.on("calibrated", function() {
+//   console.log("calibrated");
+// });
+//
+// motion.on("change", function() {
+//   lcd.clear().print(Math.random()*100);
+// });
