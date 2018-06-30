74b8d02d657a6216623070fe5521334d00f1f987
krokowski0x <krokowski.dx@gmail.com>
2018-06-30 11:28:41 +0200

    Updated modes to use lcd instead of console

--- modes/AccountMode.js
+++ modes/AccountMode.js
@@ -1,13 +1,27 @@
 const GDAX = require('gdax');
+// Secret login data for my GDAX account (secrets.js is in .gitignore - sorry not sorry)
 const { passPhrase, APIKey, secret } = require('./secrets');
 const apiURI = 'https://api.gdax.com';
+let currencies = '';
+let prices = '';
 
 function AccountMode() {
   const authenticatedClient = new GDAX.AuthenticatedClient(APIKey, secret, passPhrase, apiURI);
+  // Get account data
   authenticatedClient.getAccounts()
     .then(data => {
-      for (let entry of data)
-        console.log(entry.currency, Number(entry.balance).toFixed(3));
+      // For each currency of available
+      for (let entry of data) {
+        // Currency names, BTC, ETH etc.
+        currencies += entry.currency;
+        // Prices in the same order
+        prices += Number(entry.balance).toFixed(3);
+      }
+    })
+    .then(() => {
+      lcd.clear()
+        .cursor(0,0).print(currencies)
+        .cursor(0,1).print(prices)
     })
     .catch(error => {
       // TODO: Error handling
--- modes/SingleCurrencyMode.js
+++ modes/SingleCurrencyMode.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const apiURI = 'https://api.gdax.com/products/';
 
 function SingleCurrencyMode(args) {
+  // Get currency names from args
   args = args.filter(arg => arg.length === 3);
   const baseCurrency = args[0];
   const mainCurrency = args[1];
@@ -11,7 +12,9 @@ function SingleCurrencyMode(args) {
   fetch(`${apiURI}${mainCurrency}-${baseCurrency}/book?level=1`)
     .then(res => res.json())
   	.then(json => console.log(mainCurrency, json.asks[0][0]))
-    .catch(e => console.log(e));
+    .catch(error => {
+      // TODO: Error handling
+  });
 };
 
 module.exports = SingleCurrencyMode;
